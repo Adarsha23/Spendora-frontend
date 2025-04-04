30486472cd118d3281213af1aa7f35e62b0347e7
Adarsha23 <144573496+Adarsha23@users.noreply.github.com>
2025-04-04 19:47:37 +0545

    Final codes for homepage and logs page with update app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import React from "react";
-import { BrowserRouter, Routes,Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Logs from "./pages/Logs";
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Routes>
-        <Route path="/home" element={<Home />} />
+      <Routes>
+        {/* Update the path to root ("/") */}
+        <Route path="/" element={<Home />} />
         <Route path="/logs" element={<Logs />} />
-
-    </Routes>
-    
+      </Routes>
     </BrowserRouter>
   );
 };
 
 export default App;
-
-
-
