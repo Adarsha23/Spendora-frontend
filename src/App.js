7ed5820b9334136438c9f27a365a398657044fd4
Ojisha <ojishakc@gmail.com>
2025-04-09 12:05:39 +0545

    Updated App.js for Calendar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Page components
 import Home from "./pages/Home";
 import Logs from "./pages/Logs";
+import CalendarPage from "./pages/CalendarPage"; // Updated to reflect the moved calendar files
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Update the path to root ("/") */}
+        {/* Home page */}
         <Route path="/" element={<Home />} />
+
+        {/* Logs page */}
         <Route path="/logs" element={<Logs />} />
+
+        {/* Calendar page */}
+        <Route path="/calendar" element={<CalendarPage />} />
       </Routes>
     </BrowserRouter>
   );
