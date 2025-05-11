ca017b13ebf658c08dda2cbc592f7d8e907a7c35
Adarsha23 <144573496+Adarsha23@users.noreply.github.com>
2025-05-11 12:21:16 +0545

    Upated the header so its text can be changed dynamically

--- src/home/Header.js
+++ src/home/Header.js
@@ -4,10 +4,11 @@ import "./Header.css";
 const Header = ({ title }) => {
   return (
     <div className="header">
-      <h2 className="overview-title">Overview</h2>
+      <h2 className="overview-title">{title}</h2> {/* Use the dynamic title */}
     </div>
   );
 };
 
 export default Header;
+
 
